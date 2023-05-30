13d4d767ee2c239c466e844ccb3dad9c097150bd
Hanmilton Berrezueta <hberrezu@pichincha.com>
2023-05-29 23:21:40 -0500

    Base socket server mon with webflux

--- src/main/java/com/mon/socket/server/ServerApplication.java
+++ src/main/java/com/mon/socket/server/ServerApplication.java
@@ -1,61 +1,22 @@
 package com.mon.socket.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.mon.socket.server"
+        }
+)
 public class ServerApplication
 {
 
-    public static void main(String[] args) throws
-                                           IOException
+    public static void main(String[] args)
     {
-        int          port         = 8888;
-        ServerSocket serverSocket = new ServerSocket(port);
-
-        while (true)
-        {
-            System.out.println("Calling server");
-            Socket socket = serverSocket.accept();
-            new Thread(() -> handleClient(socket)).start();
-        }
-    }
-
-    private static void handleClient(Socket socket)
-    {
-        try
-        {
-            PrintWriter out = new PrintWriter(
-                    socket.getOutputStream(),
-                    true
-            );
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String request;
-            while ((request = in.readLine()) != null)
-            {
-                String response = processRequest(request);
-                out.println(response);
-            }
-
-            out.close();
-            in.close();
-            socket.close();
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    private static String processRequest(String request)
-    {
-        return "Server response: " + request.toUpperCase();
+        SpringApplication.run(
+                ServerApplication.class,
+                args
+        );
     }
 
 }
